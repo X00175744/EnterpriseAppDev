6f6d256770b023ab9d153ca2c18b24ab5d79cf74
luke geraghty <x00175744@mytudublin.ie>
2023-12-13 21:45:33 +0000

    final

---
+++ CA3_X00175744/AreaOfEffect.cs
@@ -0,0 +1,45 @@
+﻿namespace CA3
+{
+	// Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+	public class Item
+	{
+		public int id { get; set; }
+		public int rarity { get; set; }
+		public int value { get; set; }
+		public int carryLimit { get; set; }
+		public string name { get; set; }
+		public string description { get; set; }
+	}
+
+	public class Protection
+	{
+		public List<Item> items { get; set; }
+		public List<Skill> skills { get; set; }
+	}
+
+	public class Recovery
+	{
+		public List<string> actions { get; set; }
+		public List<Item> items { get; set; }
+	}
+
+	public class Ailments
+	{
+		public int id { get; set; }
+		public string name { get; set; }
+		public string description { get; set; }
+		public Recovery recovery { get; set; }
+		public Protection protection { get; set; }
+	}
+
+	public class Skill
+	{
+		public int id { get; set; }
+		public string name { get; set; }
+		public string description { get; set; }
+	}
+
+
+
+}
+
